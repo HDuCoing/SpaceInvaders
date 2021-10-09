6a47533c54dcb445a3bbfffe6830910cde38ee8e
Holly <71049594+HDuCoing@users.noreply.github.com>
2021-10-09 22:01:11 +1300

    Update Alien.java

--- src/com/company/Alien.java
+++ src/com/company/Alien.java
@@ -1,10 +1,6 @@
 package com.company;
 
-import java.awt.Rectangle;
-import java.io.Serializable;
 
-// this class can be much improved, better encapsulation
-// draw itself, update itself etc. etc.
 public class Alien{
     public final double created;
     double x;
